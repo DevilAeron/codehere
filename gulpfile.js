9a192c5d06d9f0ddc6ef0ce171843d29fe0a0957
DevilAeron <guptavedant2312@gmail.com>
2021-07-25 12:23:21 +0530

    make it better

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const gulp = require("gulp");
+const gap = require("gulp-append-prepend");
+
+gulp.task("licenses", async function () {
+  // this is to add Creative Tim licenses in the production mode for the minified js
+  gulp
+    .src("build/static/js/*chunk.js", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* Codehere
+=========================================================
+
+* Coded by Vedant Gupta
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  gulp
+    .src("build/index.html", { base: "./" })
+    .pipe(
+      gap.prependText(`<!--
+
+=========================================================
+* Codehere
+=========================================================
+
+* Coded by Vedant Gupta
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production mode for the minified css
+  gulp
+    .src("build/static/css/*chunk.css", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* codehere
+=========================================================
+
+* Coded by Vedant Gupta
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
